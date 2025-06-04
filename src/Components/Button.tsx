40a4134592945c4c1ceb243c65551cdc73ec91c9
regestea23 <amirhossein5starstar@outlook.com>
2025-06-04 16:49:06 +0330

    Add Button component with icon and click handler support

---
+++ src/Components/Button.tsx
@@ -0,0 +1,25 @@
+﻿import {IconDefinition} from "@fortawesome/free-solid-svg-icons";
+import {MouseEventHandler} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+type ButtonProps = {
+    className: string;
+    icon?: IconDefinition;
+    iconClassName?: string;
+    title?: string;
+    onButtonClick?: MouseEventHandler;
+};
+
+export default function Button(props: ButtonProps) {
+    return (
+        <button className={props.className} onClick={props.onButtonClick}>
+            {props.icon && (
+                <span className={props.iconClassName}>
+                        <FontAwesomeIcon icon={props.icon}/>
+                    </span>
+            )}
+            {props.title && <span>{props.title}</span>}
+
+        </button>
+    );
+}
